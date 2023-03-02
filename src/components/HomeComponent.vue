d84b77cfa74b02f0e96835fc7714f81183eb0d68
francescotrudu <francescotrudu84@gmail.com>
2023-03-02 18:37:25 +0100

    Add HomeComponent

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'HomeComponent',
+  data() {
+    return {
+      project: [],
+    }
+  },
+  methods: {
+
+  },
+}
+
+</script>
+
+<template>
+  <h1>prova</h1>
+</template>
+
+<style scoped></style>
