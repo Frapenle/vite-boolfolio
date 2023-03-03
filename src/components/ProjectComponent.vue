04031ab2a114c7018ae5e9a1abd836db7746ef78
francescotrudu <francescotrudu84@gmail.com>
2023-03-03 09:47:48 +0100

    Add card images path

--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -45,10 +45,13 @@ export default {
         <div class="cards-container d-flex gap-2">
 
           <div class="card" style="width: 18rem;" v-for="project in projects">
-            <img :src="project.preview" class="card-img-top" :alt="project.name">
+            <img v-if="!project.preview.endsWith('placeholder-300x300.jpeg')"
+              :src="'http://127.0.0.1:8000/storage/' + project.preview" class="img-fluid" :alt="project.name">
+            <img v-else :src="'http://127.0.0.1:8000/storage/img/uploads/placeholder-300x300.jpeg'" class="img-fluid"
+              :alt="project.name">
             <div class="card-body d-flex flex-column">
               <h5 class="card-title">Name: {{ project.name }}</h5>
-              <p class="card-text">{{ project.description.substr(0, 150) }}...
+              <p class="card-text">Description: {{ project.description.substr(0, 150) }}...
               </p>
             </div>
             <div class="justify-content-end p-3"><a href="#" class="btn btn-primary disable">Open</a></div>
